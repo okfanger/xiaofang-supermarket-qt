b599a76f71082099bc2ccfd5129fb344de413aca
okfang <lovefyj616@foxmail.com>
2022-07-01 05:45:23 +0800

    feat: add buywidget

--- buywidget.cpp
+++ buywidget.cpp
@@ -1,14 +1,49 @@
 #include "buywidget.h"
 #include "ui_buywidget.h"
-
+#include <QSqlTableModel>
+#include <QSqlRecord>
+#include <QDebug>
+#include <QMessageBox>
+#include <QSqlError>
+#include "readonlydelegate.h"
 buywidget::buywidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::buywidget)
 {
     ui->setupUi(this);
-}
+    model = new QSqlTableModel(this);
+    model->setTable("goods");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
 
+    model->select();
+
+    for (int i=0;i<4;i++){
+        model->setHeaderData(i, Qt::Horizontal, tr(tableColumnConfig[i]));
+    }
+    ui->tableView->setModel(model);
+    for (int i=0;i<4;i++){ ui->tableView->setColumnWidth(i, tableColumnWidth[i]); }
+    ui->tableView->setItemDelegateForColumn(0, new ReadOnlyDelegate(this)); // 编号列不可修改
+    ui->tableView->setItemDelegateForColumn(1, new ReadOnlyDelegate(this)); // 创建时间列不可修改
+    ui->tableView->setItemDelegateForColumn(2, new ReadOnlyDelegate(this)); // 创建时间列不可修改
+    ui->tableView->setItemDelegateForColumn(3, new ReadOnlyDelegate(this));
+}
 buywidget::~buywidget()
 {
     delete ui;
+}
+//进货
+void buywidget::on_pushButton_clicked()
+{
+    int curRow = ui->tableView->currentIndex().row();
+    auto oldValue = model->record(curRow).value(QString("store")).toInt();
+    auto newValue = oldValue + 1;
+    model->setData(model->index(curRow, 3), QVariant(newValue));//重新设置当前单元格内容
+}
+//出货
+void buywidget::on_pushButton_2_clicked()
+{
+    int curRow = ui->tableView->currentIndex().row();
+    auto oldValue = model->record(curRow).value(QString("store")).toInt();
+    auto newValue = (oldValue - 1>0)?oldValue - 1:0;
+    model->setData(model->index(curRow, 3), QVariant(newValue));//重新设置当前单元格内容
 }
--- buywidget.h
+++ buywidget.h
@@ -2,7 +2,8 @@
 #define BUYWIDGET_H
 
 #include <QWidget>
-
+#include <QSqlQuery>
+#include <QSqlTableModel>
 namespace Ui {
 class buywidget;
 }
@@ -15,8 +16,16 @@ public:
     explicit buywidget(QWidget *parent = 0);
     ~buywidget();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::buywidget *ui;
+    QSqlTableModel* model;
+    const char* tableColumnConfig[4] = {"编号", "商品名称", "描述", "库存"};
+    const int tableColumnWidth[4] = {27,87,143,44};
 };
 
 #endif // BUYWIDGET_H
